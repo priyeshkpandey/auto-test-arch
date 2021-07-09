6c8c4e0ee3b20ef9b2942506980bf23ae2b729e3
Priyesh Pandey <priyeshpandey@Priyeshs-MacBook-Pro.local>
2021-07-09 18:13:52 +0530

    initial commit

---
+++ mock-service/src/main/java/com/mock/app/main/ApplicationRunner.java
@@ -0,0 +1,9 @@
+package com.mock.app.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.mock.app")
+public class ApplicationRunner {
+}
